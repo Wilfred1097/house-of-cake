60675c4c7ebb162f5ddd07d4aab4a30169c8f050
Wilfredo D. Catalan <catalanwilfredo97@gmail.com>
2024-12-30 00:54:10 +0800

    added vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+// vite.config.js
+export default {
+    server: {
+      host: '0.0.0.0',  // Allow external connections
+      port: 5173         // You can also specify a port here, but 5173 is the default
+    }
+  }
+  
